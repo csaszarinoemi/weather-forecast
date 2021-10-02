2c0f4285d45aa081c7384434e183d84d97cac87e
Csaszari Noemi <noemi.csaszari@schibsted.com>
2021-10-02 15:23:20 +0200

    Wire in weather API

--- src/App.js
+++ src/App.js
@@ -1,11 +1,71 @@
-import React from 'react'
-import { MapComponent } from './MapComponent'
+import React, {useState, useCallback} from 'react';
+import axios from 'axios';
+import { MapComponent } from './MapComponent';
+
 const App = () => {
+  const [dataApi, setDataApi] = useState({ isLoadingApi: false, errorApi: false, data: {} })
+
+  const fetchData = useCallback(async (coord) => {
+    setDataApi({ ...dataApi, isLoadingApi: true })
+    try {
+      const { data } = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${coord.lat}&lon=${coord.lng}&units=metric&appid=094080442761c029747e0c8ec0ed88e6`)
+      setDataApi({ ...dataApi, isLoadingApi: false, errorApi: false, data })
+      console.log(data)
+    } catch (error) {
+      setDataApi({ ...dataApi, isLoadingApi: false, errorApi: error?.message })
+    }
+  }, [dataApi])
+
+  const handleOnClickMap = (coords) => {
+    fetchData(coords)
+  }
+
   return (
     <>
-      <MapComponent />  
+      <MapComponent 
+        onClickMap={handleOnClickMap}
+      />
+     <div>
+      <h2>Weather and location data</h2>
+      {dataApi.errorApi && <div><span>⚠ {dataApi.errorApi} ⚠</span></div>}
+      <div>
+        <div>
+          {
+            dataApi.isLoadingApi
+              ? <span>Cargando...</span>
+              : (
+                <span>
+                  <span>{dataApi.data?.name}</span>
+                  <br />
+                  <span>
+                    {dataApi.data?.main?.temp || 0} °C
+                  </span>
+                </span>
+              )
+          }
+        </div>
+        {
+          (Object.keys(dataApi.data).length > 0 && !dataApi.isLoadingApi) && (
+            <div>
+              <ul>
+                {
+                  Object.keys(dataApi.data?.main).map((elem, i) => {
+                    return (<li key={i}>{elem}: {dataApi.data?.main[elem]}</li>)
+                  })
+                }
+                {
+                  Object.keys(dataApi.data?.coord).length > 0 && Object.keys(dataApi.data?.coord).map((elem, i) => {
+                    return (<li key={i}>{elem}: {dataApi.data?.coord[elem]}</li>)
+                  })
+                }
+              </ul>
+            </div>
+          )
+        }
+      </div>
+    </div>
     </>
   )
 }
 
-export default App;
+export default App
--- src/MapComponent.js
+++ src/MapComponent.js
@@ -2,11 +2,12 @@ import React, { useState } from 'react'
 
 import { Map, Marker, Popup, TileLayer } from 'react-leaflet'
 
-export const MapComponent = ({ positionDefault = [47.497913, 19.040236] }) => {
-  const [markers, setMarkers] = useState([[4.660779178475431, -74.08494168529754]])
+export const MapComponent = ({ positionDefault = [47.497913, 19.040236], onClickMap }) => {
+  const [markers, setMarkers] = useState([[47.497913, 19.040236]])
   const handleClick = (e) => {
     console.log(e.latlng)
     setMarkers([...markers, [e.latlng.lat, e.latlng.lng]])
+    onClickMap(e.latlng)
   }
 
     return (
